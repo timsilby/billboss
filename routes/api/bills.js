e1777284c0c94e2294d6905340384d9d14789e07
timsilby <69242373+timsilby@users.noreply.github.com>
2021-01-29 22:06:44 +1000

    Added comment.

--- routes/api/bills.js
+++ routes/api/bills.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const billsController = require("../../controllers/billsController");
 
 // These routes match /api/bills. Imported into api-routes.js.
-
-// Generic get route. Req and res are automatically passed through to the handlers.
+// Req and res are automatically passed through to the handlers in most cases.
 router.route("/")
 	.get(function (req, res) {
 		if (req.query.id) {billsController.getBillById(req, res)}
@@ -12,17 +11,5 @@ router.route("/")
 	.post(billsController.createBill)
 	.put(billsController.updateBill);
 
-
-// // Matches with "/api/books"
-// router.route("/")
-//   .get(booksController.findAll)
-//   .post(booksController.create);
-
-// // Matches with "/api/books/:id"
-// router
-//   .route("/:id")
-//   .get(booksController.findById)
-//   .put(booksController.update)
-//   .delete(booksController.remove);
 
 module.exports = router;
