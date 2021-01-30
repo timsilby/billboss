20c372cfa8a7a85e5b6e7a4fdf284f13582e20d3
timsilby <69242373+timsilby@users.noreply.github.com>
2021-01-30 18:06:53 +1000

    Updated model to generate fullName on save.

--- models/User.js
+++ models/User.js
@@ -12,11 +12,17 @@ const userSchema = new Schema({
 		type: String,
 		trim: true
 	},
-	fullName: String,
 	email: {
 		type: String,
 		trim: true,
 		required: true
+	},
+	fullName: {
+		type: String,
+		trim: true,
+		default: function() {
+			return `${this.firstName} ${this.lastName || ""}`
+		}
 	}
 
 });
