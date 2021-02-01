1dc9559192c73868de69470469b3cd95fe7939ce
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-01 18:58:45 +1000

    Changed model name properties to match what they'll be in the URL.

---
+++ models/dbModels.js
@@ -0,0 +1,12 @@
+
+const dbModels = {
+
+	accounts: require("./Account"),
+	bills: require("./Bill"),
+	billsets: require("./Billset"),
+	transactions: require("./Transaction"),
+	users: require("./User")
+
+}
+
+module.exports = dbModels;
