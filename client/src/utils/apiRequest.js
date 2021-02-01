5056e4b2646358509f0b3cb3847abbab18340803
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-02 05:25:30 +1000

    Removed baseurl config.

--- client/src/utils/apiRequest.js
+++ client/src/utils/apiRequest.js
@@ -6,45 +6,38 @@ import getTokenHeader from "./getTokenHeader"
 // the request.
 const apiRequest = {
 
-	baseUrl: "http://localhost:3001/api",
-
 	getEntries: async function (url) {
 
-		const requestURL = `${this.baseUrl}${url}`
 		const tokenHeader = await getTokenHeader();
-		return await axios.get(requestURL, tokenHeader);
+		return await axios.get(url, tokenHeader);
 
 	},
 
 	getEntryById: async function (url) {
 
-		const requestURL = `${this.baseUrl}${url}`
 		const tokenHeader = await getTokenHeader();
-		return await axios.get(requestURL, tokenHeader);
+		return await axios.get(url, tokenHeader);
 
 	},
 
 	createEntry: async function(url, data) {
 
-		const requestURL = `${this.baseUrl}${url}`
 		const tokenHeader = await getTokenHeader();
-		return await axios.post(requestURL, data, tokenHeader);
+		return await axios.post(url, data, tokenHeader);
 
 	},
 
 	updateEntry: async function(url, data) {
 
-		const requestURL = `${this.baseUrl}${url}`
 		const tokenHeader = await getTokenHeader();
-		return await axios.put(requestURL, data, tokenHeader);
+		return await axios.put(url, data, tokenHeader);
 
 	},
 
 	deleteEntry: async function (url) {
 
-		const requestURL = `${this.baseUrl}${url}`
 		const tokenHeader = await getTokenHeader();
-		return await axios.delete(requestURL, tokenHeader);
+		return await axios.delete(url, tokenHeader);
 
 	},
 
