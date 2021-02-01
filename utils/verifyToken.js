e3e2c212ed6b4572ef11610017afe2208685d519
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-02 04:14:00 +1000

    Parsed env variable so the private key works on heroku.

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -8,7 +8,7 @@ const serviceAccount = {
 	"type": process.env.FIREBASE_SA_TYPE,
 	"project_id": process.env.FIREBASE_SA_PROJECT_ID,
 	"private_key_id": process.env.FIREBASE_SA_PRIVATE_KEY_ID,
-	"private_key": process.env.FIREBASE_SA_PRIVATE_KEY,
+	"private_key": process.env.FIREBASE_SA_PRIVATE_KEY.replace(/\\n/g, "\n"),
 	"client_email": process.env.FIREBASE_SA_CLIENT_EMAIL,
 	"client_id": process.env.FIREBASE_SA_CLIENT_ID,
 	"auth_uri": process.env.FIREBASE_SA_CLIENT_ID,
