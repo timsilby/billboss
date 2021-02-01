adb79d22c76bf7aa22d05318745eefd8acfea71d
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-02 04:25:05 +1000

    Testing db access on heroku

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import apiRequest from "../utils/apiRequest";
-// import { useAuth } from "../utils/useAuth"
+import { useAuth } from "../utils/useAuth"
 
 
 const Dashboard = () => {
 
 	const [data, setData] = useState();
-	// const auth = useAuth();
-	// const fireUid = auth.user.uid;
+	const auth = useAuth();
+	const fireUid = auth.user.uid;
 
-	// const doc = {
-	// 	"amount": 66
-	// }
+// 	const doc = {
+// 		"title": "Internet",
+// 		"amount": 75,
+// 	"fireUid": fireUid
+// }
 
 	useEffect(() => {
 
@@ -20,12 +22,12 @@ const Dashboard = () => {
 
 	const dosomething = async () => {
 
-		// const dbdata = await apiRequest.getEntries(`/bills/${fireUid}`)
+		const dbdata = await apiRequest.getEntries(`/bills/${fireUid}`)
 		// const dbdata = await apiRequest.createEntry(`/bills/${fireUid}`, doc)
 		// const dbdata = await apiRequest.getEntryById(`/bills?id=60177615651d8c5918ceabb9`, doc)
 		// const dbdata = await apiRequest.updateEntry(`/bills?id=60176cc03e35081734d0c0c7`, doc)
-		const dbdata = await apiRequest.deleteEntry(`/bills?id=60177615651d8c5918ceabb9`)
-		setData(JSON.stringify(dbdata.data.deletedCount));
+		// const dbdata = await apiRequest.deleteEntry(`/bills?id=60177615651d8c5918ceabb9`)
+		setData(JSON.stringify(dbdata.data));
 
 	}
 
