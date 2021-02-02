19fb95c45f3aae95a71f57cc3d16299b49505c49
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-02 20:42:31 +1000

    Removed cors import

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const cors = require("cors");
 const routes = require("./routes/all-routes");
 const verifyToken = require("./utils/verifyToken");
 
