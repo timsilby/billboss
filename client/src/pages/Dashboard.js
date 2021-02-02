f6775eb647f01ff98dd8b74abaad21e66be13279
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-02 20:04:32 +1000

    Deployment test

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,18 +1,17 @@
 import { useState, useEffect } from "react";
 import apiRequest from "../utils/apiRequest";
-import { useAuth } from "../utils/useAuth"
+// import { useAuth } from "../utils/useAuth"
 
 
 const Dashboard = () => {
 
 	const [data, setData] = useState();
-	const auth = useAuth();
-	const fireUid = auth.user.uid;
+	// const auth = useAuth();
+	// const fireUid = auth.user.uid;
 
 // 	const doc = {
-// 		"title": "Internet",
-// 		"amount": 75,
-// 	"fireUid": fireUid
+// 		"title": "Food",
+// 		"amount": 120,
 // }
 
 	useEffect(() => {
@@ -22,9 +21,9 @@ const Dashboard = () => {
 
 	const dosomething = async () => {
 
-		const dbdata = await apiRequest.getEntries(`/api/bills/${fireUid}`)
-		// const dbdata = await apiRequest.createEntry(`/api/bills/${fireUid}`, doc)
-		// const dbdata = await apiRequest.getEntryById(`/api/bills?id=60177615651d8c5918ceabb9`, doc)
+		const dbdata = await apiRequest.getEntries(`/api/bills`);
+		// const dbdata = await apiRequest.createEntry(`/api/bills`, doc)
+		// const dbdata = await apiRequest.getEntryById(`/api/bills?id=601841c24fa9490f9c8e18c9`)
 		// const dbdata = await apiRequest.updateEntry(`/api/bills?id=60176cc03e35081734d0c0c7`, doc)
 		// const dbdata = await apiRequest.deleteEntry(`/api/bills?id=60177615651d8c5918ceabb9`)
 		setData(JSON.stringify(dbdata.data));
