b082670e0c37ac212dc5764d6f4257cebdb70adb
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-04 04:38:05 +1000

    Added redirect for logged in user hitting the landing page.

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,15 +1,25 @@
 import Container from "@material-ui/core/Container";
+import { Redirect } from "react-router-dom";
+import { useAuth } from "../../utils/useAuth";
 import Header from "./Header";
 import LoginForm from "./LoginForm";
 
 
 const Landing = () => {
 
+	const auth = useAuth();
+
 	return (
+
+		auth.user
+		?
+		<Redirect to="/dashboard" />
+		:
 		<Container component="main" maxWidth="xs">
 			<Header />
 			<LoginForm />
 		</Container>
+
 	);
 
 }
