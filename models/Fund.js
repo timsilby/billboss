31ba186d76494ef163716080ff8fcc4344abff02
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-04 05:07:27 +1000

    Changed name of model

--- models/Fund.js
+++ models/Fund.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const accountSchema = new Schema({
+const fundSchema = new Schema({
 
 	startBalance: {
 		type: Number,
@@ -14,6 +14,6 @@ const accountSchema = new Schema({
 
 });
 
-const Fund = mongoose.model("Account", accountSchema);
+const Fund = mongoose.model("Fund", fundSchema);
 
 module.exports = Fund;
