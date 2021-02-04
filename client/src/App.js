fb7805cd11c24673efbf1ef88e6aa5b7601dbea8
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-05 03:19:30 +1000

    Removed some comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,5 @@
 import { ProvideAuth } from "./utils/useAuth";
 import Routes from "./components/Routes";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Dashboard from "./components/Dashboard/Dashboard";
-// import Landing from "./components/Landing/Landing";
-// import PrivateRoute from "./components/PrivateRoute";
-// import Bills from "./components/Bills/Bills";
-// import Budget from "./components/Budget/Budget";
-// import Reports from "./components/Reports/Reports";
-
 
 function App() {
 
