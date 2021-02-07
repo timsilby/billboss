57f217218ec4089a80b5217cd64ef0ed140a4a99
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-07 14:33:06 +1000

    Added some testing code.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import apiRequest from "../../utils/apiRequest";
 import { useAuth } from "../../utils/useAuth";
 import AppbarDrawer from "../AppBarDrawer/AppbarDrawer";
+import dayjs from "dayjs";
 
 const Dashboard = () => {
 
@@ -13,8 +14,13 @@ const Dashboard = () => {
 
 	const dosomething = async () => {
 
-		const dbdata = await apiRequest.getEntries(`/api/bills`);
-		// const dbdata = await apiRequest.createEntry(`/api/bills`, doc)
+		const dbdata = await apiRequest.getEntries("/api/bills");
+		console.log(dayjs("2021-02-01T17:59:33.656Z"));
+		console.log(dayjs("2021-02-01T17:59:33.656+00:00"));
+		console.log(dayjs("2021-02-01T17:59:33.656Z") === dayjs("2021-02-01T17:59:33.656+00:00"));
+		const d1 = dayjs("2021-02-01T17:59:33.656Z");
+		const d2 = dayjs("2021-02-01T17:59:33.656+00:00");
+		if (d1 === d2) {console.log("equal");}		// const dbdata = await apiRequest.createEntry(`/api/bills`, doc)
 		// const dbdata = await apiRequest.getEntryById(`/api/bills?id=601841c24fa9490f9c8e18c9`)
 		// const dbdata = await apiRequest.updateEntry(`/api/bills?id=60176cc03e35081734d0c0c7`, doc)
 		// const dbdata = await apiRequest.deleteEntry(`/api/bills?id=60177615651d8c5918ceabb9`)
@@ -29,10 +35,11 @@ const Dashboard = () => {
 
 	return (
 		<div>
-			<AppbarDrawer />
+			<AppbarDrawer>
 			<h1>Dashboard</h1>
 			<button onClick={dosomething}>Do something</button>
 			{data}
+			</AppbarDrawer>
 		</div>
 	);
 
