877ad6620183b67605dea69599095a22121ae7ef
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-08 17:38:37 +1000

    Created bill summary screen and table

---
+++ client/src/components/Dashboard/BillTable.js
@@ -0,0 +1,40 @@
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+
+const BillTable = ({ bills, billtype }) => {
+
+	return (
+		<TableContainer component={Paper}>
+			<Table aria-label={`${billtype} bills table"`}>
+				<TableHead>
+					<TableRow>
+						<TableCell>Bill Name</TableCell>
+						<TableCell align="left">Due Date</TableCell>
+						<TableCell align="right">Amount</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{bills.map((bill) => (
+						<TableRow key={bill._id}>
+							<TableCell component="th" scope="row">
+								{bill.title}
+							</TableCell>
+							<TableCell align="left">{bill.date}</TableCell>
+							<TableCell align="right">{bill.amount}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+
+	);
+
+}
+
+export default BillTable;
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,30 +1,25 @@
 import { useState, useEffect } from "react";
-import apiRequest from "../../utils/apiRequest";
 import { useAuth } from "../../utils/useAuth";
-import AppbarDrawer from "../AppBarDrawer/AppbarDrawer";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import apiRequest from "../../utils/apiRequest";
+import AppbarDrawer from "../AppBarDrawer/AppbarDrawer";
+import BillSummary from "./BillSummary";
+import Typography from "@material-ui/core/Typography";
+
+dayjs.extend(utc)
 
 const Dashboard = () => {
 
 	const [data, setData] = useState();
+
+
 	const auth = useAuth();
 	console.log("dashboard");
 	console.log(auth.user);
 
 
 	const dosomething = async () => {
-
-		const dbdata = await apiRequest.getEntries("/api/bills");
-		console.log(dayjs("2021-02-01T17:59:33.656Z"));
-		console.log(dayjs("2021-02-01T17:59:33.656+00:00"));
-		console.log(dayjs("2021-02-01T17:59:33.656Z") === dayjs("2021-02-01T17:59:33.656+00:00"));
-		const d1 = dayjs("2021-02-01T17:59:33.656Z");
-		const d2 = dayjs("2021-02-01T17:59:33.656+00:00");
-		if (d1 === d2) {console.log("equal");}		// const dbdata = await apiRequest.createEntry(`/api/bills`, doc)
-		// const dbdata = await apiRequest.getEntryById(`/api/bills?id=601841c24fa9490f9c8e18c9`)
-		// const dbdata = await apiRequest.updateEntry(`/api/bills?id=60176cc03e35081734d0c0c7`, doc)
-		// const dbdata = await apiRequest.deleteEntry(`/api/bills?id=60177615651d8c5918ceabb9`)
-		setData(JSON.stringify(dbdata.data));
 
 	}
 
@@ -36,9 +31,10 @@ const Dashboard = () => {
 	return (
 		<div>
 			<AppbarDrawer>
-			<h1>Dashboard</h1>
-			<button onClick={dosomething}>Do something</button>
-			{data}
+				<Typography variant="h5" component="h1">Dashboard</Typography>
+				{/* <button onClick={dosomething}>Do something</button>
+				{data} */}
+				<BillSummary />
 			</AppbarDrawer>
 		</div>
 	);
