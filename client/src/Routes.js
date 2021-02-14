3d574c04ce49aaee76698c8c33751a9eb203a218
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-14 11:20:02 +1000

    Moved routes file

---
+++ client/src/Routes.js
@@ -0,0 +1,49 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useAuth } from "./utils/useAuth";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Landing from "./components/Landing/Landing";
+import Bills from "./components/Bills/Bills";
+import Budget from "./components/Budget/Budget";
+import Reports from "./components/Reports/Reports";
+import Signup from "./components/Landing/Signup";
+import Account from "./components/Account/Account";
+
+
+function Routes() {
+
+	const auth = useAuth();
+	const user = auth.user;
+
+	return (
+
+		<Router>
+			<Switch>
+				<Route exact path="/dashboard">
+					{user ? <Dashboard /> : <Landing />}
+				</Route>
+				<Route exact path="/bills">
+					{user ? <Bills /> : <Landing />}
+				</Route>
+				<Route exact path="/account">
+					{user ? <Account /> : <Landing />}
+				</Route>
+				<Route exact path="/budget">
+					{user ? <Budget /> : <Landing />}
+				</Route>
+				<Route exact path="/reports">
+					{user ? <Reports /> : <Landing />}
+				</Route>
+				<Route exact path="/signup">
+					<Signup />
+				</Route>
+				<Route path="/">
+					<Landing />
+				</Route>
+			</Switch>
+		</Router>
+
+	)
+
+}
+
+export default Routes;
