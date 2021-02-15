f0cb54c93fecbe06e9ff2f12897b4a764fe1a52f
timsilby <69242373+timsilby@users.noreply.github.com>
2021-02-15 13:37:03 +1000

    Added dialog to inform login failure

--- client/src/components/Landing/LoginForm.js
+++ client/src/components/Landing/LoginForm.js
@@ -6,9 +6,14 @@ import Grid from "@material-ui/core/Grid";
 import Link from "@material-ui/core/Link";
 import { Link as RouterLink } from "react-router-dom";
 import Box from "@material-ui/core/Box";
+import LoginResultDialog from "./LoginResultDialog";
+import { useState } from "react";
+
 
 
 const LoginForm = () => {
+
+	const [dialogOpen, setDialogOpen] = useState(false);
 
 	// Get any state that was passed from referring page. If it exists
 	// get the referrer.
@@ -19,6 +24,8 @@ const LoginForm = () => {
 	// Grab some hooks.
 	const auth = useAuth();
 	const history = useHistory();
+
+	const toggleDialog = () => setDialogOpen(!dialogOpen);
 
 	// Login process
 	const handleSubmit = async (event) => {
@@ -39,7 +46,10 @@ const LoginForm = () => {
 
 		}
 		catch (err) {
+
+			toggleDialog();
 			console.error(err);
+
 		}
 
 	}
@@ -47,55 +57,58 @@ const LoginForm = () => {
 
 	return (
 
-		<form autoComplete="false" onSubmit={handleSubmit}>
-			<TextField
-				variant="outlined"
-				required
-				fullWidth
-				margin="normal"
-				id="email"
-				label="Email Address"
-				name="email"
-				autoFocus
+		<>
+			<form autoComplete="false" onSubmit={handleSubmit}>
+				<TextField
+					variant="outlined"
+					required
+					fullWidth
+					margin="normal"
+					id="email"
+					label="Email Address"
+					name="email"
+					autoFocus
 				/>
-			<TextField
-				variant="outlined"
-				required
-				fullWidth
-				margin="normal"
-				id="password"
-				name="password"
-				label="Password"
-				type="password"
-			/>
-			<Box mt={2}>
-				<Grid
-					container
-					alignItems="center"
-					justify="space-between"
-				>
-					<Grid item>
-						<Button
-							type="submit"
-							variant="contained"
-							color="primary"
-						>
-							Sign In
+				<TextField
+					variant="outlined"
+					required
+					fullWidth
+					margin="normal"
+					id="password"
+					name="password"
+					label="Password"
+					type="password"
+				/>
+				<Box mt={2}>
+					<Grid
+						container
+						alignItems="center"
+						justify="space-between"
+					>
+						<Grid item>
+							<Button
+								type="submit"
+								variant="contained"
+								color="primary"
+							>
+								Sign In
 						</Button>
+						</Grid>
+						<Grid item>
+							<Link
+								variant="body2"
+								component={RouterLink}
+								to="/Signup"
+								color="secondary"
+							>
+								{"No account? Sign Up Here"}
+							</Link>
+						</Grid>
 					</Grid>
-					<Grid item>
-						<Link
-							variant="body2"
-							component={RouterLink}
-							to="/Signup"
-							color="secondary"
-						>
-							{"No account? Sign Up Here"}
-						</Link>
-					</Grid>
-				</Grid>
-			</Box>
-		</form>
+				</Box>
+			</form>
+			<LoginResultDialog open={dialogOpen} toggleDialog={toggleDialog} />
+		</>
 
 	)
 
---
+++ client/src/components/Landing/LoginResultDialog.js
@@ -0,0 +1,57 @@
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		backgroundColor: theme.palette.error.main,
+		paddingLeft: theme.spacing(6),
+		paddingRight: theme.spacing(6),
+		paddingTop: theme.spacing(1),
+		paddingBottom: theme.spacing(2),
+	},
+	actionBar: {
+		justifyContent: "center",
+		paddingTop: theme.spacing(2),
+		paddingBottom: theme.spacing(2)
+	},
+}));
+
+
+const LoginResultDialog = ({ open, toggleDialog }) => {
+
+	const classes = useStyles();
+
+	return (
+
+		<div>
+			<Dialog
+				open={open}
+				onClose={toggleDialog}
+				maxWidth="xs"
+			>
+				<DialogContent className={classes.root}>
+					<DialogContentText variant="h6" component="h1">
+						Login Failed
+					</DialogContentText>
+					<DialogContentText>
+						Please try again
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions className={classes.actionBar}>
+					<Button className={classes.button} onClick={toggleDialog} color="primary" variant="contained">
+						Close
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
+
+	);
+
+}
+
+export default LoginResultDialog;
